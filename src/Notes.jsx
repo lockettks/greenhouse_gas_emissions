4fe84c029ed16c2825fb82613436b56d88716d4d
kimberlymathieu <lockettks@gmail.com>
2025-03-01 18:29:46 -0500

    Cleanup notes

--- src/Notes.jsx
+++ src/Notes.jsx
@@ -30,20 +30,20 @@ export const Notes = () => {
         </li>
       </ol>
       <p>
-        If this were a production app, I would be implementing the additional functionality:
-        <ul>Automated testing (unit and component level)</ul>
-        <ul>Better error handling</ul>
-        <ul>Improve the UX for if a user wants to select a single year instead of a range</ul>
-        <ul>At least one additional visualization, especially when a single year is selected in the future</ul>
+        If this were a production app, I would be implementing the following additional functionality:
+        <ul>Automated testing (unit and component level).</ul>
+        <ul>Better error handling.</ul>
+        <ul>Improve the UX for if a user wants to select a single year instead of a range.</ul>
+        <ul>At least one additional visualization, especially when a single year is selected.</ul>
         <ul>
           Improve the responsiveness of the visualization. The chart as of right now is at the charting library's limits
           for responsiveness, so additional tooling would be required.
         </ul>
-        <ul>Improve the accessibility of the application</ul>
-        <ul>Add an empty state, error state, and loading state</ul>
+        <ul>Improve the accessibility of the application.</ul>
+        <ul>Add an empty state, error state, and loading state.</ul>
         <ul>
           Depending on other project factors such as engineering team size, experience level, and the app roadmap, I
-          would consider using Typescript.
+          would consider using Typescript in order to improve maintainability and dependability.
         </ul>
       </p>
     </Card>
